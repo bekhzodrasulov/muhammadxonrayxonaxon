70a2046bae7014dcc6fb001bb668f1f6b7ecd61d
BekhzodRasulov <bekhzodr2001@gmail.com>
2025-07-11 23:41:24 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const playButton = document.getElementById("playButton");
+const playIcon = document.getElementById("playIcon");
+const bgMusic = document.getElementById("bgMusic");
+
+playButton.addEventListener("click", () => {
+  if (bgMusic.paused) {
+    bgMusic.play();
+    playIcon.src = "images/pouse.png";
+    playIcon.alt = "Pause";
+  } else {
+    bgMusic.pause();
+    playIcon.src = "images/play.png";
+    playIcon.alt = "Play";
+  }
+});
+
+function updateCountdown() {
+  const weddingDate = new Date("2025-07-24T19:30:00");
+  const now = new Date();
+  const diff = weddingDate - now;
+
+  if (diff <= 0) {
+    document.getElementById("days").textContent = 0;
+    document.getElementById("hours").textContent = 0;
+    document.getElementById("minutes").textContent = 0;
+    document.getElementById("seconds").textContent = 0;
+    return;
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / 1000 / 60) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
